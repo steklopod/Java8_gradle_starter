458b96dc1580ff5f3d01053bfd7b1c2a0af27d04
Dmitry.K <steklopod@gmail.com>
2018-03-05 15:21:51 +0300

    Initial commit

---
+++ src/main/java/ru/steklopod/GradleStarterApplication.java
@@ -0,0 +1,12 @@
+package ru.steklopod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleStarterApplication.class, args);
+	}
+}
