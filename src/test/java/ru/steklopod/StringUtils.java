a2196d095c09bfc8f67f9303e40c28a7116894ce
Dima Koltovich <dmitry.koltovich@stoloto.ru>
2018-05-16 17:36:20 +0300

    StringUtils

--- src/test/java/ru/steklopod/StringUtils.java
+++ src/test/java/ru/steklopod/StringUtils.java
@@ -1,8 +1,6 @@
 package ru.steklopod;
 
-import io.github.glytching.junit.extension.random.RandomBeansExtension;
 import lombok.extern.slf4j.Slf4j;
-import name.falgout.jeffrey.testing.junit.mockito.MockitoExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.runner.JUnitPlatform;
@@ -11,22 +9,25 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.apache.commons.lang3.StringUtils.*;
+import static org.apache.commons.lang3.StringUtils.countMatches;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 @SpringBootTest
 @RunWith(JUnitPlatform.class)
-@ExtendWith({MockitoExtension.class, SpringExtension.class, RandomBeansExtension.class})
+@ExtendWith(SpringExtension.class)
 @Transactional
 class StringUtils {
 
+    //http://www.baeldung.com/string-processing-commons-lang
     @Test
     void stringUtils() {
         String string = "abba.com";
-        boolean contained1 = org.apache.commons.lang3.StringUtils.containsAny(string, 'a', 'b', 'c');
-        boolean contained2 = org.apache.commons.lang3.StringUtils.containsAny(string, 'x', 'y', 'z');
-        boolean contained3 = org.apache.commons.lang3.StringUtils.containsAny(string, "abc");
-        boolean contained4 = org.apache.commons.lang3.StringUtils.containsAny(string, "xyz");
+        boolean contained1 = containsAny(string, 'a', 'b', 'c');
+        boolean contained2 = containsAny(string, 'x', 'y', 'z');
+        boolean contained3 = containsAny(string, "abc");
+        boolean contained4 = containsAny(string, "xyz");
 
         assertTrue(contained1);
         assertFalse(contained2);
@@ -34,11 +35,16 @@ class StringUtils {
         assertFalse(contained4);
 
         String s2 = "welcome to www.steklopod.com";
-        int charNum = org.apache.commons.lang3.StringUtils.countMatches(s2, 'w');
-        int stringNum = org.apache.commons.lang3.StringUtils.countMatches(s2, "com");
+        int charNum = countMatches(s2, 'w');
+        int stringNum = countMatches(s2, "com");
 
         assertEquals(4, charNum);
         assertEquals(2, stringNum);
+
+        String originalString = "www.baeldung.com";
+        String reversedString = reverseDelimited(originalString, '.');
+
+        assertEquals("com.baeldung.www", reversedString);
     }
 
 }
