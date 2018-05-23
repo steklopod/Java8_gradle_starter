893b516cc2c4c46ab74830edd3e4f5b5945140c1
Dima Koltovich <dmitry.koltovich@stoloto.ru>
2018-05-23 15:57:04 +0300

    human Readable Duration

---
+++ src/test/java/ru/steklopod/Java8ExamplesTest.java
@@ -0,0 +1,60 @@
+package ru.steklopod;
+
+import io.github.glytching.junit.extension.random.RandomBeansExtension;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import name.falgout.jeffrey.testing.junit.mockito.MockitoExtension;
+import org.apache.commons.lang3.text.WordUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+import ru.steklopod.repositories.UserDao;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@SpringBootTest
+@RunWith(JUnitPlatform.class)
+@ExtendWith({MockitoExtension.class, SpringExtension.class, RandomBeansExtension.class})
+@Transactional
+class Java8ExamplesTest {
+
+    @Autowired
+    private UserDao userDao;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    @SneakyThrows
+    void intStream() {
+        long now = System.currentTimeMillis();
+        Thread.sleep(2000);
+        long duration = System.currentTimeMillis() - now;
+        String s = toHumanReadableDuration(duration);
+        System.err.println(s);
+    }
+
+    private static final List<TimeUnit> timeUnits = Arrays.asList(TimeUnit.DAYS, TimeUnit.HOURS, TimeUnit.MINUTES,
+            TimeUnit.SECONDS);
+
+    public static String toHumanReadableDuration(final long millis) {
+        final StringBuilder builder = new StringBuilder();
+        long acc = millis;
+        for (final TimeUnit timeUnit : timeUnits) {
+            final long convert = timeUnit.convert(acc, TimeUnit.MILLISECONDS);
+            if (convert > 0) {
+                builder.append(convert).append(' ').append(WordUtils.capitalizeFully(timeUnit.name())).append(", ");
+                acc -= TimeUnit.MILLISECONDS.convert(convert, timeUnit);
+            }
+        }
+        return builder.substring(0, builder.length() - 2);
+    }
+}
