e8a6820e8009e8e94807d0a1113ac8102df81919
Dima Koltovich <steklopod@gmail.com>
2018-09-11 17:20:17 +0300

    getArchive method ready [v- 0.2]

--- src/main/java/ru/steklopod/service/MockService.java
+++ src/main/java/ru/steklopod/service/MockService.java
@@ -1,13 +1,22 @@
 package ru.steklopod.service;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 @RestController
 public class MockService {
 
-    @RequestMapping("/getArchive")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    private String filename = "archive.rar";
+
+    @GetMapping(value = "/getArchive")
+    public InputStreamResource index() throws FileNotFoundException {
+        File file = new File(filename);
+        return new InputStreamResource(new FileInputStream(file));
     }
+
 }
