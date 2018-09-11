a824f0f66bf35d387ec5fb05305c305548941e99
Dima Koltovich <steklopod@gmail.com>
2018-09-11 12:36:12 +0300

    Dependencies updated

--- src/main/java/ru/steklopod/Starter.java
+++ src/main/java/ru/steklopod/Starter.java
@@ -11,9 +11,7 @@ public class Starter extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Starter.class);
     }
-
     public static void main(String[] args) {
         SpringApplication.run(Starter.class, args);
     }
-
 }
--- src/test/java/ru/steklopod/InitTest.java
+++ src/test/java/ru/steklopod/InitTest.java
@@ -3,24 +3,42 @@ package ru.steklopod;
 import io.github.glytching.junit.extension.random.RandomBeansExtension;
 import lombok.extern.slf4j.Slf4j;
 import name.falgout.jeffrey.testing.junit.mockito.MockitoExtension;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.stream.IntStream;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Slf4j
 @SpringBootTest
 @RunWith(JUnitPlatform.class)
 @ExtendWith({MockitoExtension.class, SpringExtension.class, RandomBeansExtension.class})
+@AutoConfigureMockMvc
 class InitTest {
 
-    @DisplayName("MariaDb: сохранение ")
-//    @Disabled
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+    }
+
+    //    @Disabled
     @Test
     void intStream() {
         IntStream.iterate(0, i -> i + 2).limit(3);    // > 0, 2, 4
